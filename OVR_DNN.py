afc74b1b7d36560bcd03cccbec75981b0aaa0eac
Kiran <bhattacharyyakiran12@gmail.com>
2021-08-08 23:47:27 -0400

    add all files -- still testing

---
+++ OVR_DNN.py
@@ -0,0 +1,188 @@
+import numpy as np
+import pandas as pd
+import pickle
+import glob 
+from sklearn.model_selection import train_test_split
+from skmultilearn.model_selection import iterative_train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import roc_auc_score
+
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.neighbors import RadiusNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.linear_model import RidgeClassifierCV
+from sklearn.ensemble import StackingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.experimental import enable_hist_gradient_boosting
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.multioutput import ClassifierChain
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_decomposition import CCA
+
+from xgboost import XGBClassifier
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+
+from imblearn.combine import SMOTEENN
+from imblearn.ensemble import EasyEnsembleClassifier
+from imblearn.ensemble import BalancedBaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from imblearn.ensemble import BalancedRandomForestClassifier
+from imblearn.ensemble import RUSBoostClassifier
+
+from imblearn.pipeline import make_pipeline as make_pipeline_with_sampler
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler 
+
+from timeit import default_timer
+
+
+import matplotlib.pyplot as plt
+
+import glob
+
+class OVR_DNN:
+    
+    def __init__(self, X_train=None, y_train=None, filename=None):
+        self._X_train = X_train
+        self._y_train = y_train
+        self._filename = filename
+        if self._X_train is not None:
+            self.train_base_models()
+            self.train_stk_models()
+        if self._filename is not None:
+            print('Loading models...')
+            self.load_models()
+        
+        
+        
+    def train_base_models(self):
+        self.split_data()
+        estimators = self.get_models()
+        first_one = True
+        feat_imp_sums = np.zeros(self._X_train.shape[1])
+        for pair in estimators:
+            print('Training base model', pair[0])
+            pair[1].fit(self._X_train, self._y_train)
+            for est in pair[1].estimators_:
+                if hasattr(est[1], 'feature_importances_'):
+                    feat_imp_sums += est[1].feature_importances_
+        self._base_models = estimators
+        self._imp_feats = feat_imp_sums > np.mean(feat_imp_sums)
+        
+        
+        
+        
+    def get_models(self):
+        base_lr = make_pipeline(StandardScaler(), LogisticRegression(class_weight='balanced'))
+        ovr_lr = OneVsRestClassifier(base_lr)
+
+        base_rf = make_pipeline_with_sampler(RandomUnderSampler(), RandomForestClassifier(n_jobs=-1))
+        ovr_rf = OneVsRestClassifier(base_rf)
+
+        base_et = make_pipeline_with_sampler(RandomUnderSampler(), ExtraTreesClassifier(n_jobs=-1))
+        ovr_et = OneVsRestClassifier(base_et)
+
+        base_gbc = make_pipeline_with_sampler(RandomUnderSampler(), HistGradientBoostingClassifier())
+        ovr_gbc = OneVsRestClassifier(base_gbc)
+
+        estimators = [('lr', ovr_lr),
+              ('rf', ovr_rf),
+              ('et', ovr_et),
+              ('gbc', ovr_gbc)]
+        return estimators
+    
+    
+    def split_data(self):
+        print('Splitting data into training and validation set to train DNNs...')
+        X_train, y_train, X_val, y_val = iterative_train_test_split(self._X_train, 
+                                                              self._y_train, 
+                                                              test_size = 0.35)
+        self._X_train = X_train
+        self._y_train = y_train
+        self._X_val = X_val
+        self._y_val = y_val
+        print('Train data:', X_train.shape)
+        print('Train labels:', y_train.shape)
+        print('Val data:', X_val.shape)
+        print('Val labels:', y_val.shape)
+            
+            
+    def train_stk_models(self):
+        print('Training stacking model on validation set...')
+        for i,model in enumerate(self._base_models):
+            print('  Getting probabilities for validation set...')
+            this_y_prob = model[1].predict_proba(self._X_val)
+            if i == 0:
+                y_prob = this_y_prob
+            else:
+                y_prob = np.concatenate((y_prob, this_y_prob), axis=1)
+                
+        stk_brf = BalancedRandomForestClassifier(n_estimators=400, n_jobs=-1)
+        ovr_stk_brf = OneVsRestClassifier(stk_brf)
+        ovr_stk_brf.fit(y_prob, self._y_val)
+        self._stk_model = ovr_stk_brf
+    
+    
+    
+    def predict(self, X_test):
+        for i,model in enumerate(self._base_models):
+            this_y_prob = model[1].predict_proba(X_test)
+            if i == 0:
+                y_prob = this_y_prob
+            else:
+                y_prob = np.concatenate((y_prob, this_y_prob), axis=1)
+        
+        y_pred_test = self._stk_model.predict(y_prob)
+        return y_pred_test
+    
+    
+    
+    def predict_proba(self, X_test):
+        for i,model in enumerate(self._base_models):
+            this_y_prob = model[1].predict_proba(X_test)
+            if i == 0:
+                y_prob = this_y_prob
+            else:
+                y_prob = np.concatenate((y_prob, this_y_prob), axis=1)
+        
+        y_pred_test = self._stk_model.predict_proba(y_prob)
+        return y_pred_test
+    
+    
+    
+    def save_models(self, filename):
+        model_dict = {
+            'base_models': self._base_models,
+            'imp_feats': self._imp_feats,
+            'stk_model': self._stk_model
+        }
+        stk_filename = filename.split('.pick')[0] + '_ovr_imb_models.pickle'
+        with open(stk_filename, 'wb') as handle:
+            pickle.dump(model_dict, handle)
+        handle.close()
+        
+    
+    
+    
+    def load_models(self):
+        # load in base dnn models
+        file_hash = self._filename.split('.pick')[0] + '_ovr_imb_models.pickle'
+        with open(file_hash, 'rb') as handle:
+            model_dict = pickle.load(handle)
+        handle.close()
+        self._base_models = model_dict['base_models']
+        self._imp_feats = model_dict['imp_feats']
+        self._stk_model = model_dict['stk_model']
+    
+    
+    
---
+++ team_code.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+
+# Edit this script to add your team's training code.
+# Some functions are *required*, but you can edit most parts of the required functions, remove non-required functions, and add your own functions.
+
+################################################################################
+#
+# Imported functions and variables
+#
+################################################################################
+
+# Import functions. These functions are not required. You can change or remove them.
+from helper_code import *
+import numpy as np, os, sys, joblib
+import pandas as pd
+from slice_dice import Slice_Dice
+from Train_Autoencoder import The_Autoencoder
+from Encode_Shape import Encode_Shape
+from sklearn.neighbors import KNeighborsClassifier
+from OVR_DNN import OVR_DNN
+import pickle
+
+# Define the Challenge lead sets. These variables are not required. You can change or remove them.
+twelve_leads = ('I', 'II', 'III', 'aVR', 'aVL', 'aVF', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6')
+six_leads = ('I', 'II', 'III', 'aVR', 'aVL', 'aVF')
+four_leads = ('I', 'II', 'III', 'V2')
+three_leads = ('I', 'II', 'V2')
+two_leads = ('I', 'II')
+lead_sets = (twelve_leads, six_leads, four_leads, three_leads, two_leads)
+
+################################################################################
+#
+# Training model function
+#
+################################################################################
+
+# Train your model. This function is *required*. You should edit this function to add your code, but do *not* change the arguments of this function.
+def training_code(data_directory, model_directory):
+    # Find header and recording files.
+    print('Finding header and recording files...')
+
+    header_files, recording_files = find_challenge_files(data_directory)
+    num_recordings = len(recording_files)
+
+    if not num_recordings:
+        raise Exception('No data was provided.')
+
+    # Create a folder for the model if it does not already exist.
+    if not os.path.isdir(model_directory):
+        os.mkdir(model_directory)
+
+    # Extract features and labels from dataset.
+    print('Extracting features and labels...')
+    
+    # data extraction parameters
+    desire_rate = 50 #Hz, desired sampling rate
+    time_window = 5 #sec, window of time of ekg to look in
+    overlap = 0.25 # proportion of overlap between time windows
+
+    # loop through all recordings
+    for i in range(num_recordings):
+        print('    {}/{}...'.format(i+1, num_recordings))
+
+        # Load header and recording.
+        this_header = load_header(header_files[i])
+        recording = load_recording(recording_files[i])
+        this_recording = recording.T # transpose data time is rows and columns are features
+
+        # get labels and frequency
+        this_freq = get_frequency(this_header)
+        this_labels = get_labels(this_header)
+        
+        # shape data and labels for single recording
+        print('  slicing and dicing data...')
+        slicer = Slice_Dice(this_recording, this_freq)
+        chunks = slicer.process_chunks(desire_rate, 
+                                       time_window, 
+                                       overlap, 
+                                       this_header)
+        chunk_labels = [this_labels]*len(chunks)
+        group = [recording_files[i]]*len(chunks)
+        print('     found', len(chunks), '5 second windows in this file...')
+        
+        # combine all recordings
+        if i == 0:
+            all_chunks = chunks
+            all_labels = chunk_labels
+            all_groups = group
+        else:
+            all_chunks.extend(chunks)
+            all_labels.extend(chunk_labels)
+            all_groups.extend(group)
+
+    print('Training the encoder model...')
+    my_encoder = The_Autoencoder(all_chunks, all_groups)
+
+    # Train a model for each lead set.
+    for leads in lead_sets:
+        print('Training model for {}-lead set: {}...'.format(len(leads), ', '.join(leads)))
+
+        shaper = Encode_Shape(my_encoder, all_chunks, leads, 
+                          chunk_labels=all_labels)
+        features, labels = shaper.get_shaped_output()
+        print('  found features of shape:', features.shape)
+        print('  found labels of shape:', labels.shape)
+
+        # Train the model.
+        my_ovr = OVR_DNN(X_train=features, y_train=labels)
+
+        # Save the model.
+        save_model(model_directory, leads, shaper_object, my_ovr, leads)
+
+################################################################################
+#
+# Running trained model function
+#
+################################################################################
+
+# Run your trained model. This function is *required*. You should edit this function to add your code, but do *not* change the arguments of this function.
+def run_model(model, header, recording):
+    leads = model['leads']
+    classifier = model['classifier']
+    my_encoder = model['encoder']
+    
+    # Load features
+    desire_rate = 50 #Hz, desired sampling rate
+    time_window = 5 #sec, window of time of ekg to look in
+    overlap = 0.95 # proportion of overlap between time windows
+    this_recording = recording.T
+    this_freq = get_frequency(header)
+    slicer = Slice_Dice(this_recording, this_freq)
+    chunks = slicer.process_chunks(desire_rate, 
+                                       time_window, 
+                                       overlap, 
+                                       header)
+    shaper = Encode_Shape(my_encoder, chunks, leads, test=True)
+    features = shaper.get_shaped_output()
+
+    # Predict labels and probabilities
+    many_predicts = classifier.predict(features)
+    probabilities = np.mean(many_predicts, axis=0)
+    print(probabilities)
+    
+    labels = np.zeros(len(probabilities)).astype(np.int)
+    labels[probabilities >= 0.5] = 1
+    print(labels)
+    
+    classes = shaper._scored_labels
+    print(classes)
+    return classes, labels, probabilities
+
+################################################################################
+#
+# File I/O functions
+#
+################################################################################
+
+# Save a trained model. This function is not required. You can change or remove it.
+def save_model(model_directory, leads, shaper_object, my_ovr, leads):
+    filename = get_model_filename(leads)
+    filename = os.path.join(model_directory, filename)
+    # save encoder
+    filename_tf = filename.split('.pick')[0] + '_encoder.pickle'
+    shaper_object._encoder.save(filename_tf)
+    # save ovr model files
+    my_ovr.save_models(filename)
+    # save leads
+    model_dict = {
+        'leads': leads,
+    }
+    with open(filename, 'wb') as handle:
+        pickle.dump(model_dict, handle)
+    handle.close()
+    
+
+# Load a trained model. This function is *required*. You should edit this function to add your code, but do *not* change the arguments of this function.
+def load_model(model_directory, leads):
+    filename = get_model_filename(leads)
+    filename = os.path.join(model_directory, filename)
+    # load encoder
+    filename_tf = filename.split('.pick')[0] + '_encoder.pickle'
+    encoder_object = The_Autoencoder(encoder_filename=filename_tf)
+    # load ovr models
+    my_ovr = OVR_DNN(filename=filename)
+    # load leads
+    with open(filename, 'rb') as handle:
+        saved_dict = pickle.load(handle)
+    handle.close()
+    model_dict = {
+        'encoder': encoder_object,
+        'leads': saved_dict['leads'],
+        'classifier': my_ovr
+    }
+    return model_dict
+
+# Define the filename(s) for the trained models. This function is not required. You can change or remove it.
+def get_model_filename(leads):
+    sorted_leads = sort_leads(leads)
+    return 'model_' + '-'.join(sorted_leads) + '.pickle'
